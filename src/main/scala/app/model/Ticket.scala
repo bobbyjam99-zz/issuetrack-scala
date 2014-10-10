d4ed8bedc69c897ca038325ac21c66f8f6e61a37
Tomokazu IMAI <bobbyjam99@gmail.com>
2014-10-10 20:52:19 +0900

    add description attribute.

--- src/main/scala/app/model/Ticket.scala
+++ src/main/scala/app/model/Ticket.scala
@@ -9,4 +9,4 @@ sealed trait Ticket {
 
 case class Issue(id: Long, title: String, status: TicketStatus = TicketStatus.Open) extends Ticket
 
-case class Bug(id: Long, title: String, status: TicketStatus = TicketStatus.Open) extends Ticket
+case class Bug(id: Long, title: String, description: String, status: TicketStatus = TicketStatus.Open) extends Ticket
