fd5642b7bacef3ba9f23f5371409c0c3e5f1f35b
Tomokazu IMAI <bobbyjam99@gmail.com>
2014-10-10 20:53:13 +0900

    create TicketRepo object.

---
+++ src/main/scala/app/repo/TicketRepo.scala
@@ -0,0 +1,13 @@
+package app.repo
+
+import app.model.TicketStatus.Fixed
+import app.model.{Bug, Issue, Ticket}
+
+object TicketRepo {
+  private var map: Map[Long, Ticket] = Map(
+    1L -> Issue(1L, "画面を作る"),
+    2L -> Bug(2L, "ジョジョのセリフが違う", "ピザ食べたいジョ"),
+    3L -> Issue(3L, "データベースを作る", Fixed),
+    4L -> Bug(4L, "画像が違う", "ぬこ画像はよ!!", Fixed)
+  )
+}
