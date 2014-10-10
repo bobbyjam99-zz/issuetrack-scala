1d100dc78f92ce4af5c6dd2ed2502f795e7ff6f9
Tomokazu IMAI <bobbyjam99@gmail.com>
2014-10-10 21:40:16 +0900

    create TicketId

--- src/main/scala/app/repo/TicketRepo.scala
+++ src/main/scala/app/repo/TicketRepo.scala
@@ -2,9 +2,10 @@ package app.repo
 
 import app.model.TicketStatus.Fixed
 import app.model.{Bug, Issue, Ticket}
+import repo.TicketId
 
 object TicketRepo {
-  private var map: Map[Long, Ticket] = Map(
+  private var map: Map[TicketId, Ticket] = Map(
     1L -> Issue(1L, "画面を作る"),
     2L -> Bug(2L, "ジョジョのセリフが違う", "ピザ食べたいジョ"),
     3L -> Issue(3L, "データベースを作る", Fixed),
